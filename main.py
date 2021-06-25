307b351da843400d3604495f101205c5341f50b9
yrenyi1212 <yrenyi1212@gmail.com>
2021-06-25 14:34:32 +0800

    update

--- main.py
+++ main.py
@@ -2,5 +2,4 @@
 import pytest
 
 if __name__ == '__main__':
-    # pytest.main(['-s', 'testcases/', '-m download', '--alluredir=./tmp/my_allure_results '])
     pytest.main(['-s', 'testcases/', '--alluredir=./tmp/my_allure_results '])
